07439724ace9d6a486f986f72f9983ef3a58a8c8
hermens16 <hermens16@hotmail.com>
2026-03-06 17:51:41 -0300

    Atualiza├º├úo autom├ítica IPTV

--- atualizar.bat
+++ atualizar.bat
@@ -1,13 +1,17 @@
-cd C:\iptv-br\scripts
+@echo off
 
-py pluto.py
+cd C:\iptv-br\scripts\pluto_tv_scraper
+
+node index.js
+
+copy plutotv_br.m3u8 C:\iptv-br\listas\pluto.m3u8
+
+cd C:\iptv-br\scripts
 
 py juntar_listas.py
 
 cd C:\iptv-br
 
 git add .
-
 git commit -m "Atualização automática IPTV"
-
 git push origin main
--- scripts/pluto.py
+++ scripts/pluto.py
@@ -1,12 +1,11 @@
 import json
 import urllib.request
 
-url = "https://api.pluto.tv/v2/channels?start=0&stop=1000&region=BR&deviceType=web"
+url = "https://service-channels.clusters.pluto.tv/v1/guide?start=0&stop=24&country=BR"
 
 headers = {
     "User-Agent": "Mozilla/5.0",
-    "Accept": "application/json",
-    "Referer": "https://pluto.tv/",
+    "Accept": "application/json"
 }
 
 req = urllib.request.Request(url, headers=headers)
@@ -14,19 +13,21 @@ req = urllib.request.Request(url, headers=headers)
 with urllib.request.urlopen(req) as response:
     data = json.loads(response.read().decode())
 
+canais = data["channels"]
+
 with open("../listas/pluto.m3u8", "w", encoding="utf-8") as f:
 
     f.write("#EXTM3U\n")
 
-    for canal in data:
+    for canal in canais:
 
         nome = canal["name"]
-        logo = canal.get("solidLogoPNG", {}).get("path", "")
         canal_id = canal["_id"]
+        logo = canal.get("colorLogoPNG", {}).get("path", "")
 
         stream = f"https://service-stitcher.clusters.pluto.tv/stitch/hls/channel/{canal_id}/master.m3u8"
 
         f.write(f'#EXTINF:-1 tvg-id="{canal_id}" tvg-logo="{logo}" group-title="PLUTO",{nome}\n')
         f.write(stream + "\n")
 
-print("Lista Pluto atualizada")
+print("Lista Pluto gerada com sucesso!")
