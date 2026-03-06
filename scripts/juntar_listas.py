e357dbed405c4e60a2645f4c156c53bada8705c8
hermens16 <hermens16@hotmail.com>
2026-03-06 10:43:54 -0300

    Atualiza├º├úo autom├ítica IPTV

--- scripts/juntar_listas.py
+++ scripts/juntar_listas.py
@@ -1,31 +1,20 @@
-arquivos = [
-"../listas/pluto.m3u8",
-"../listas/plex.m3u8",
-"../listas/samsung.m3u8",
-"../listas/lg.m3u8",
-"../listas/runtime.m3u8"
+import os
+
+listas = [
+    "../listas/pluto.m3u8"
 ]
 
 saida = "../listas/lista_completa.m3u8"
 
-with open(saida,"w",encoding="utf-8") as out:
+with open(saida, "w", encoding="utf-8") as final:
+    final.write("#EXTM3U\n")
 
-    out.write("#EXTM3U\n")
-
-    for arq in arquivos:
-
-        try:
-
-            with open(arq,"r",encoding="utf-8") as f:
-
+    for lista in listas:
+        if os.path.exists(lista):
+            with open(lista, "r", encoding="utf-8") as f:
                 for linha in f:
-
                     if linha.startswith("#EXTM3U"):
                         continue
-
-                    out.write(linha)
-
-        except:
-            pass
+                    final.write(linha)
 
 print("Lista completa criada")
