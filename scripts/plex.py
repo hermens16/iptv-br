a5738cdfc32c73ed3512d739fcd835a38d12683a
hermens16 <hermens16@hotmail.com>
2026-03-07 13:12:42 -0300

    Atualiza├º├úo autom├ítica IPTV

--- scripts/plex.py
+++ scripts/plex.py
@@ -1,6 +1,20 @@
 import urllib.request
 
 url = "https://iptv-org.github.io/iptv/countries/br.m3u"
-urllib.request.urlretrieve(url, "../listas/plex.m3u8")
+arquivo_temp = "../listas/_temp_br.m3u"
 
-print("Canais Brasil atualizados")
+urllib.request.urlretrieve(url, arquivo_temp)
+
+with open(arquivo_temp, "r", encoding="utf-8") as f:
+    linhas = f.readlines()
+
+saida = []
+
+for linha in linhas:
+    if "#EXTINF" in linha or linha.startswith("http"):
+        saida.append(linha)
+
+with open("../listas/plex.m3u8", "w", encoding="utf-8") as f:
+    f.writelines(saida)
+
+print("Lista Plex Brasil atualizada!")
--- scripts/runtime.py
+++ scripts/runtime.py
@@ -1,6 +1,20 @@
 import urllib.request
 
-url = "https://iptv-org.github.io/iptv/categories/movies.m3u"
-urllib.request.urlretrieve(url, "../listas/runtime.m3u8")
+url = "https://iptv-org.github.io/iptv/countries/br.m3u"
+arquivo_temp = "../listas/_temp_br.m3u"
 
-print("Runtime atualizado")
+urllib.request.urlretrieve(url, arquivo_temp)
+
+with open(arquivo_temp, "r", encoding="utf-8") as f:
+    linhas = f.readlines()
+
+saida = []
+
+for linha in linhas:
+    if "#EXTINF" in linha or linha.startswith("http"):
+        saida.append(linha)
+
+with open("../listas/runtime.m3u8", "w", encoding="utf-8") as f:
+    f.writelines(saida)
+
+print("Lista Runtime Brasil atualizada!")
